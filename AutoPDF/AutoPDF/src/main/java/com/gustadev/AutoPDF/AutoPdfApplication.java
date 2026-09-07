14a1b396fa432ec4636cfa1b090c6f8402ec164b
Gusta-s <gustavo13fonseca@gmail.com>
2026-09-07 15:03:51 -0300

    initial commit

---
+++ AutoPDF/AutoPDF/src/main/java/com/gustadev/AutoPDF/AutoPdfApplication.java
@@ -0,0 +1,13 @@
+package com.gustadev.AutoPDF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPdfApplication.class, args);
+	}
+
+}
---
+++ AutoPDF/AutoPDF/src/test/java/com/gustadev/AutoPDF/AutoPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustadev.AutoPDF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
